c64f09f3150af5defc90edbb432ae437fffe2fa2
Dat T Nguyen <n.dat@outlook.com>
2025-08-13 21:37:59 +0200

    fix partion orion

--- run_impute_real_60_eur_067x.sh
+++ run_impute_real_60_eur_067x.sh
@@ -3,8 +3,7 @@
 #SBATCH --nodes=1                
 #SBATCH --job-name=imp_real067   
 #SBATCH --mem=4G                
-#SBATCH --partition=gpu
-#SBATCH --mail-user=nguyen.thanh.dat@nmbu.no
+#SBATCH --mail-user=n.dat@outlook.com
 #SBATCH --mail-type=ALL
 
 
