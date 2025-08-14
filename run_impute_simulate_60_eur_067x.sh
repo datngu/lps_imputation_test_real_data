830fd3db499cbb028456b4ac79fc08c7eb0a86c8
Dat T Nguyen <n.dat@outlook.com>
2025-08-14 17:42:43 +0200

    imp sim

---
+++ run_impute_simulate_60_eur_067x.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1                
+#SBATCH --job-name=imp_sim067   
+#SBATCH --mem=4G                
+#SBATCH --mail-user=n.dat@outlook.com
+#SBATCH --mail-type=ALL
+
+
+module load BCFtools/1.10.2-GCC-8.3.0
+module load git/2.23.0-GCCcore-9.3.0-nodocs
+#module load Nextflow/21.03
+module load nextflow/20.10.0
+module load singularity/rpm
+
+
+export NXF_SINGULARITY_CACHEDIR=/mnt/users/ngda/sofware/singularity
+
+
+nextflow run bam2vcf.nf \
+    -w 'work_bam2vcf' \
+    --bam '/mnt/ScratchProjects/Aqua-Faang/dat_projects/test_simulated_data/results/lps_067x/*.bam{,.bai}' \
+    --ref_vcf '/mnt/ScratchProjects/Aqua-Faang/dat_projects/igsr_data/ref_for_120/*.vcf.gz{,.csi}' \
+    -resume \
+    -profile cluster \
